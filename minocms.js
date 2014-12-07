4ce8f3678392fe791914ec38efacb5d47d9d9240
Marcus Longmuir <marcuslongmuir@me.com>
2014-12-07 00:49:15 +0000

    Added jQuery plugins and support for grouping of requests

--- minocms.js
+++ minocms.js
@@ -21,12 +21,8 @@ function MinoCMS(options) {
     cms.data_server.use(errorHandler());
     cms.data_server.use(bodyParser());
     cms.data_server.use(express.static(path.join(__dirname, 'public')));
-    cms.data_server.post("/*", function(req,res) {
-        logger.log(req.body);
-        var name = req.body.name;
-        logger.log(cms.full_path + name);
-        cms.sdk.get([cms.full_path + name], function(get_err, get_res) {
-            logger.log(JSON.stringify(get_err, null, 4), get_res);
+    cms.data_server.get("/path/:path", function(req,res) {
+        cms.sdk.get([cms.full_path + req.params.path], function(get_err, get_res) {
             res.json(get_res.objects[0]);
         })
     });
@@ -34,8 +30,6 @@ function MinoCMS(options) {
 
 MinoCMS.prototype.get_config_server = function(){
     var cms = this;
-    logger.log("getting config server");
-    logger.log(cms.config_server);
     return cms.config_server;
 }
 
@@ -48,33 +42,37 @@ MinoCMS.prototype.info = function(){
     };
 }
 
-MinoCMS.prototype.create_folders = function(callback) {
-    var cms = this;
-    cms.sdk = new MinoSDK(cms.user);
-    cms.sdk.set_local_api(cms.minodb.api);
-    cms.sdk.call({
-       "function": "save",
-        "parameters": {
-            "objects" : [{
-                "name": cms.folder_name,
-                "path": "/" + cms.user + "/",
-                "folder": true
-            }]
-        } 
-    }, function(err, res) {
-        logger.log(JSON.stringify(err, null, 4), res); 
-        if (callback !== undefined) {
-            callback();
-        }   
-    })
-}
-
-MinoCMS.prototype.init = function(minodb){
+MinoCMS.prototype.init = function(minodb, callback){
     var cms = this;
     cms.minodb = minodb;
 
     minodb.internal_server().use('/cms', cms.data_server);
-    cms.create_folders();
+    
+    cms.sdk = new MinoSDK(cms.user);
+    cms.sdk.set_local_api(cms.minodb.api);
+    cms.sdk.save([{
+        "name": cms.folder_name,
+        "path": "/" + cms.user + "/",
+        "folder": true
+    }], function(err, res) {
+        logger.log(JSON.stringify(err, null, 4), res); 
+
+        cms.sdk.save_type({
+            "name":"cms_content",
+            "display_name":"CMS Content",
+            "type":"object",
+            "any": true,
+            "field_type":{
+                "type":"string"
+            }
+        }, function(err, res){
+            logger.log(JSON.stringify(err,null,4), res);
+
+            if (callback !== undefined) {
+                callback();
+            }  
+        }); 
+    });
 }
 
 module.exports = MinoCMS;
--- public/minocms.js
+++ public/minocms.js
@@ -1,6 +1,9 @@
 function MinoCMS() {
 	var cms = this;
 	cms.init_path();
+
+	cms.requests = {};
+	cms.responses = {};
 }
 
 MinoCMS.prototype.init_path = function(name, callback) {
@@ -15,15 +18,64 @@ MinoCMS.prototype.init_path = function(name, callback) {
 	}
 }
 
-MinoCMS.prototype.get = function(name, callback) {
+MinoCMS.prototype.get = function(address, callback) {
 	var cms = this;
-	var body = {
-		name: name
+
+	var split_by_dot = address.split(".");
+	var item_route = split_by_dot[0];
+	var key = split_by_dot[1];
+	
+	if(cms.responses[item_route]){
+		callback(cms.responses[item_route]);
+		return;
 	}
 
-	$.post(cms.path + "/value", body, function(res) {
-		callback(res);
+	if(cms.requests[item_route]!==undefined){
+		cms.requests[item_route].push([callback,key]);
+		return;
+	}
+
+	cms.requests[item_route] = [
+		[callback,key]
+	];
+
+	$.get(cms.path + "/path/"+item_route, function(res) {
+		cms.responses[item_route] = res;
+		for(var i = 0; i < cms.requests[item_route].length; i++){
+			var callback_pair = cms.requests[item_route][i];
+			var callback = callback_pair[0];
+			var key = callback_pair[1];
+			if(key){
+				callback(res.cms_content[key]);
+			} else {
+				callback(res);
+			}
+		}
 	})
-}
+};
+
+(function($) {
+    $.fn.minocms_text = function(address) {
+        var element = this;
+        
+        minocms.get(address, function(val){
+        	element.text(val);
+        });
+
+        return element;
+    };
+})(jQuery);
+
+(function($) {
+    $.fn.minocms_html = function(address) {
+        var element = this;
+        
+        minocms.get(address, function(val){
+        	element.html(val);
+        });
+
+        return element;
+    };
+})(jQuery);
 
 minocms = new MinoCMS();
